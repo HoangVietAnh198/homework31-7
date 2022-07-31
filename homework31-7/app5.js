49520f53cca0d06b4a4a6f40392b0d5f7d565e3c
Hoang Viet Anh <110024404+HoangVietAnh198@users.noreply.github.com>
2022-08-01 00:06:31 +0700

    Add files via upload

---
+++ homework31-7/app5.js
@@ -0,0 +1,71 @@
+// Bai 1
+// let a = prompt("Hay nhap co a")
+// let b = prompt("hay nhap so b")
+
+// if (a%b===0){
+//     alert(`${a} chia het cho ${b}`)
+// } else {
+//     alert(`${a} khong chia het cho ${b}`)
+// }
+// Bai 2
+// let a = prompt("Hay tuoi cua ban")
+// if (a>=15) {
+//     alert(`Ban du tuoi len lop 10`)
+// } else {
+//     alert(`Ban con tre con lam `)
+// }
+// Bai 3
+// let a = prompt("Hay nhap so ban muon")
+// if (a%1===0) {
+//     if (a>=0) {
+//         alert(`${a} la so lon hon 0`)
+//     } else {
+//         alert(`${a} la so nho hon 0`)
+//     }
+// } else {
+//     alert(`Ban nhap so khong phai la so nguyen`)
+// }
+// Bai 4
+// let a = Number(prompt("Hay nhap so thu nhat"))
+// let b = Number(prompt("Hay nhap so thu 2"))
+// let c = Number(prompt("Hay nhap so thu 3"))
+// if (a>b && a>c) {
+//     alert(`${a} la so lon nhat`)
+// } else if (b>a && b>c) {
+//     alert(`${b} la so lon nhat`)
+// } else {
+//     alert(`${c} la so lon nhat`)
+// }
+// Bai 5
+// let a = Number(prompt("Hay nhap diem bai kiem tra"))
+// let b = Number(prompt("Hay nhap diem thi giua ki"))
+// let c = Number(prompt("Hay nhap diem thi cuoi ki"))
+// let d = (a+b*2+c*3)/6
+// if (d>=6.5 && d<8) {
+//     alert(`Ban la hoc sinh tien tien`)
+// } else if (d>=8) {
+//     alert(`Ban la hoc sinh gioi`)
+// } else if (d>=5 && d<6.5) {
+//     alert (`Ban la hoc sinh trung binh`)
+// } else{
+//     alert(`Ban la hoc sinh yeu`)
+// }
+// Bai 6
+// let a = Number(prompt("Hay nhap luong cung cua ban"))
+// let b = Number(prompt("Hay nhap doanh so ban dat duoc trong thang"))
+// let c = a+b*1.005
+// alert(`Luong thang cong doanh so cua ban laf ${d}`)
+// Bai 7
+// let cao = Number(prompt(`bạn cao bao nhiêu cm`));
+// let nang = Number(prompt(`bạn nặng bao nhiêu kg`));
+
+// let BMI = nang/((cao*cao)/10000);
+// if (BMI < 18) {
+//     alert(`Thiếu cân`);
+// } else if (BMI >= 18,BMI <= 25) {
+//     alert(`Bình thường`);
+// } else if (BMI >=25,BMI <= 30) {
+//     alert(`Thừa cân`);
+// }  else {
+//     alert(`Béo quá!Giảm cân đi `);
+// }
